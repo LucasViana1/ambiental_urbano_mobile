4dd11519e7f4cb7c036d61469a1fd160cd234096
Lucas Viana <lucasviana112@gmail.com>
2019-11-15 21:28:21 -0300

    add rota para pagina de login

--- mobile/src/routes.js
+++ mobile/src/routes.js
@@ -1,9 +1,11 @@
 import {createAppContainer, createSwitchNavigator} from 'react-navigation';
 import Registros from './pages/Registros';
 import Cadastro from './pages/Cadastro';
+import Login from './pages/Login';
 
 export default createAppContainer(
   createSwitchNavigator({
+    Login,
     Registros,
     Cadastro,
   }),
