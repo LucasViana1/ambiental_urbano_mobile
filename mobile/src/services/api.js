26516c9a4c6e576d9d64a76ebb5b3e6b150786b3
Lucas Viana <lucasviana112@gmail.com>
2019-11-15 21:29:15 -0300

    ajuste iplocal para testes

--- mobile/src/services/api.js
+++ mobile/src/services/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://192.168.14.236:9999', //usando ip local da maquina atual
+  // baseURL: 'http://192.168.14.236:9999', //asus
+  baseURL: 'http://10.0.0.104:9999', //dell
 });
 
 export default api;
